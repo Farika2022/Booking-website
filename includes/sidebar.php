b896b5275a090c6152c459b9070f096fee4960e1
Farika Magjabeen Mohamed Farook <FARIKAMAGJABEEN2022@GMAIL.COM>
2025-03-28 17:59:27 +0100

    Add files via upload

---
+++ includes/sidebar.php
@@ -0,0 +1,79 @@
+<!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="assets/dist/img/logo-img.png" alt=" Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light">Admin MFA</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="assets/dist/img/avatar04.png" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Mohammed Farook</a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item has-treeview menu-open">
+           
+            <ul class="nav nav-treeview">
+              <li class="nav-header">Setting</li>
+              <li class="nav-item">
+                <a href="admin_index.php" class="nav-link a">
+                  <i class="nav-icon far fa-user"></i>
+                  <p>
+				  Admin Profile
+				   
+			   </p>
+                </a>
+              </li>
+			  <li class="nav-item">
+                <a href="A_registered.php" class="nav-link a">
+                  <i class="nav-icon far fa-user"></i>
+                  <p>
+				  Registered User
+			   </p>
+                </a>
+              </li>
+			  <li class="nav-item">
+                <a href="logout.php" class="nav-link a">
+                  <i class="nav-icon far fa-sign-out"></i>
+                  <p>
+				  Log Out
+			   </p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-chart-pie"></i>
+              <p>
+                Charts
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="chartjs.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>ChartJS</p>
+                </a>
+              </li>
+              
+     
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
